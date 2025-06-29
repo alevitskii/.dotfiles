31beba5bfb7a1f59bdf196279320421ed3478352
alevitskii <andrei.levitskii@proton.me>
2025-06-29 11:56:23 +0300

    Dump nvim and bash files

---
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,63 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+-- NOTE: Disabled because hlsearch is off
+-- vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- Highlight when yanking (copying) text
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('alevitskii-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Open netrw' })
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Keep the cursor in place when J
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Keep the cursor in the center when
+-- scrolling pages
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- searching
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+-- when browsing quick/location lists
+vim.keymap.set('n', '[q', '<cmd>cprevious<CR>zz')
+vim.keymap.set('n', ']q', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '[l', '<cmd>lprevious<CR>zz')
+vim.keymap.set('n', ']l', '<cmd>lnext<CR>zz')
+
+-- Paste and delete without losing current buffer
+vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete into void' })
+
+-- Substitute the word the cursor is on
+vim.keymap.set('n', '<leader>S', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Substitute the word' })
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/.config/nvim/lua/lazy/conform.lua
@@ -0,0 +1,37 @@
+return {
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format {
+            async = true,
+            lsp_format = 'never',
+          }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'never',
+      },
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        cpp = { 'clang-format' },
+        -- Conform can also run multiple formatters sequentially
+        python = { 'ruff_fix', 'ruff_organize_imports', 'ruff_format' },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    },
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
